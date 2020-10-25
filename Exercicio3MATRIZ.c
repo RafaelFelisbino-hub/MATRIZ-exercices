8df5884c0a710ed632b7c638d16dbb4d78ddb609
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-25 19:58:54 -0300

    Update Exercicio3MATRIZ.c

--- Exercicio3MATRIZ.c
+++ Exercicio3MATRIZ.c
@@ -6,19 +6,23 @@ média dos valores de cada linha da matriz.*/
 
 int main() {
     int i, j;
-    float matriz[3][3], vetor[3];
+    float matriz[3][3], vetor[3] = {0,0,0};
 
-    for (i = 0; i < 3; i++) {
-        vetor[i] = 0;
+    for (i = 0; i < 3; i++)
+    {
         for (j = 0; j < 3; j++)
         {
             printf("Digite os numeros: %d linha, %d coluna:  ", i, j);
             scanf_s("%f", &matriz[i][j]);
+            
             vetor[i] += matriz[i][j];
         }
+        
         vetor[i] /= 3;
     }
+   
     for (i = 0; i < 3; i++)
-        printf("As medias da linha %d :  %.2f\n", i, vetor[i]);
+          printf("As medias da linha %d :  %.2f\n", i, vetor[i]);
+    
     system("pause");
 }
