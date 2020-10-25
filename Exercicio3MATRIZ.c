c70583a968e3c02e64bd3d224ecd16553efed63e
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-25 14:51:08 -0300

    Update Exercicio3MATRIZ.c

--- Exercicio3MATRIZ.c
+++ Exercicio3MATRIZ.c
@@ -1,5 +1,5 @@
-/*Elabore um algoritmo que leia uma matriz 3x3 de números reais e calcule a
-média dos valores de cada linha da matriz.*/
+/*Elabore um algoritmo que leia uma matriz 3x3 de nÃºmeros reais e calcule a
+mÃ©dia dos valores de cada linha da matriz.*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +7,7 @@ média dos valores de cada linha da matriz.*/
 int main() 
 {
 	int i, j;
-	float matriz[3][3], media, media2, media3, somaTotal = 0, somaTotal2 = 0, somaTotal3 = 0;
+	float matriz[3][3], media = 0, media2 = 0, media3 = 0, somaTotal = 0, somaTotal2 = 0, somaTotal3 = 0;
 
 	for(i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++) 
@@ -15,35 +15,28 @@ int main()
 			printf("Digite os numeros: %d linha, %d coluna:  ", i, j);
 			scanf_s("%f", &matriz[i][j]);
 
-		}
-	for (i = 0; i < 1; i++)
-		for (j = 0; j < 3; j++) 
-		{
-			somaTotal += matriz[i][j];
-		}
-	media = somaTotal / j;
-	printf("A media da 1 linha e:  %.2f\n", media);
+			if (i == 0)
+			{
+				somaTotal += matriz[i][j];
+			}
 
-	for (i = 1; i < 2; i++)
-		for (j = 0; j < 3; j++)
-		{
-			somaTotal2 += matriz[i][j];
+			if (i == 1)
+			{
+				somaTotal2 += matriz[i][j];
+			}
+			
+			if (i == 2)
+			{
+				somaTotal3 += matriz[i][j];
+			}
 		}
-	media2 = somaTotal / j;
-	printf("A media da 2 linha e:  %.2f\n", media2);
-
-	for (i = 2; i < 3; i++)
-		for (j = 0; j < 3; j++)
-		{
-			somaTotal3 += matriz[i][j];
-		}
-	media3 = somaTotal / j;
-	printf("A media da 3 linha e:  %.2f\n", media3);
-
 	
-	
-	
-	
+	media = somaTotal / 3;
+	media2 = somaTotal2 / 3;
+	media3 = somaTotal3 / 3;
+	printf("A media da primeira linha e :  %.2f", media);
+	printf("A media da segunda linha e :  %.2f", media2);
+	printf("A media da terceira linha e :  %.2f", media3);
 	
 	system("pause");
 }
