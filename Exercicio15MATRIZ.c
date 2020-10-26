5fb44c4f6920df8e05cea66cc8ac7045d880f0c8
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-26 14:06:53 -0300

    Update Exercicio15MATRIZ.c

--- Exercicio15MATRIZ.c
+++ Exercicio15MATRIZ.c
@@ -1,24 +1,22 @@
-/*FaÁa um algoritmo que recebe uma matriz M (3x3) e calcula a soma dos
+/*Fa√ßa um algoritmo que recebe uma matriz M (3x3) e calcula a soma dos
 elementos da diagonal principal e a soma dos elementos da diagonal
-secund·ria da matriz.*/
+secund√°ria da matriz.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-	int matriz[3][3] = { {10,20,30}, {40,50,60}, {70,80,90} }, somaPrincipal = 0, somaSecundaria = 0;
+	int matriz[3][3] = { {10,20,30}, {40,50,60}, {70,80,90} }, somaPrincipal = 0, somaSecundaria = 0, i, j;
 
-	printf("Os elementos da diagonal primaria sao :  %i\n", matriz[0][0]);
-	printf("Os elementos da diagonal primaria sao :  %i\n", matriz[1][1]);
-	printf("Os elementos da diagonal primaria sao :  %i\n", matriz[2][2]);
+	for(i = 0; i <1; i++)
+		for (j = 0; j < 1; j++)
+		{
+			printf("Os elementos da diagonal primaria sao :  %i, %i, %i\n", matriz[0][0], matriz[1][1], matriz[2][2]);
+			printf("Os elementos da diagonal secundaria sao : %i, %i, %i\n", matriz[2][0], matriz[1][1], matriz[0][2]);
+		}
 
 	somaPrincipal = matriz[0][0] + matriz[1][1] + matriz[2][2];
-
-	printf("Os elementos da diagonal secundaria sao : %i\n", matriz[2][0]);
-	printf("Os elementos da diagonal secundaria sao :  %i\n", matriz[1][1]);
-	printf("Os elementos da diagonal secundaria sao :  %i\n", matriz[0][2]);
-
 	somaSecundaria = matriz[2][0] + matriz[1][1] + matriz[0][2];
 
 	printf("A soma da diagonal principal e:  %i\n", somaPrincipal);
