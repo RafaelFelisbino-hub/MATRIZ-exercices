df8268aba19a804e0072f38922d79b9e5db37a8b
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-26 08:00:24 -0300

    Update Exercicio5MATRIZ.c

--- Exercicio5MATRIZ.c
+++ Exercicio5MATRIZ.c
@@ -1,5 +1,5 @@
 /*Elabore um algoritmo que leia uma matriz 4x4 de inteiros e conte quantos
-números pares existem na terceira coluna da matriz.*/
+nÃºmeros pares existem na terceira coluna da matriz.*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,7 +15,7 @@ int main()
 			scanf_s("%i", &matriz[i][j]);
 		}
 	for (i = 0; i < 4; i++)
-		for (j = 0; j < 4; j++) 
+		for (j = 3; j < 4; j++) 
 		{
 			if (matriz[i][j] % 2 == 0) 
 			{
