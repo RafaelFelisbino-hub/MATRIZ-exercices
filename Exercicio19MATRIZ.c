dc8e434d7c064226c1456c1ba86b17a3388b5f33
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-27 08:37:56 -0300

    Add files via upload

---
+++ Exercicio19MATRIZ.c
@@ -0,0 +1,39 @@
+/*Faça um algoritmo que receba as vendas semanais (de um mês) de 5
+vendedores de uma loja e armazene em uma matriz. Calcule e mostre:
+a. O total de vendas do mês de cada vendedor;
+b. O total de vendas de cada semana (todos os vendedores juntos);
+c. O total de vendas do mês;*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	float matriz[5][4], somaMes[100], somaSemana = 0, totalVendas = 0;
+	int i, j;
+	
+	for(i = 0; i < 5; i++)
+		for (j = 0; j < 4; j++)
+		{
+			printf("Digite as vendas da %d loja e %d semana:  ", i + 1, j+1);
+			scanf_s("%f", &matriz[i][j]);
+		}
+	for (i = 0; i < 5; i++) 
+	{
+		somaMes[i] = 0;
+		for (j = 0; j < 4; j++)
+		{
+			somaMes[i] += matriz[i][j];
+			
+		}
+		somaSemana += somaMes[i];
+		totalVendas += somaSemana;
+	}
+	for (i = 0; i < 5; i++)
+		for (j = 0; j < 1; j++)
+			printf("O total de vendas do mes do %d vendedor e %.2f:  \n", i+1, somaMes[i]);
+			printf("O total de vendas de cada semana de todos os vendedores e %.2f:  \n", somaSemana);
+			printf("O total de vendas do mes e %.2f:  \n", totalVendas);
+
+			system("pause");
+}
