43f00a20cb3863ed4d99ccda1438dde61e6bd21e
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-27 12:50:00 -0300

    Update Exercicio20MATRIZ.c

--- Exercicio20MATRIZ.c
+++ Exercicio20MATRIZ.c
@@ -1,4 +1,4 @@
-/*Escrever um algoritmo que lê uma matriz M(4,4) e cria 2 vetores SL(4) e
+/*Escrever um algoritmo que lÃª uma matriz M(4,4) e cria 2 vetores SL(4) e
 SC(4) que contenham, respectivamente, as somas das linhas e das colunas
 de M. Escrever os vetores criados.*/
 
@@ -10,24 +10,18 @@ int main()
 	int matriz[4][4], vetorSL[4] = {0,0,0,0}, vetorSC[4] = { 0,0,0,0 }, i, j, somaSL = 0, somaSC = 0;
 
 	for (i = 0; i < 4; i++)
+	{
 		for (j = 0; j < 4; j++)
 		{
 			printf("Digite os valores da matriz [%d][%d]:  ", i, j);
 			scanf_s("%i", &matriz[i][j]);
-		}
-
-	for (i = 0; i < 4; i++)
-	{
-		for (j = 0; j < 4; j++)
-		{
 			vetorSC[i] += matriz[i][j];
 		}
-		
 		somaSC += vetorSC[i];
 		vetorSL[i] += i;
 		somaSL += vetorSL[i];
 	}
-	
+
 	printf("A soma das linhas e [%d] e a soma das colunas [%d]\n", somaSL, somaSC);
 
 	system("pause");
